35c6a3805071ac6b5e9a5978619ebb6a4f0c105a
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-04-05 12:17:49 +0300

    refactoring axios code

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,18 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '1f61c5c3e341d3551f825cc6aeed2125';
 
-export const MovieAPI = (url, param) => {
-  return axios
-    .get(`${BASE_URL}${url}?api_key=${API_KEY}&query=${param}`)
-    .then(res => res.data);
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  api_key: API_KEY,
+  language: 'en-US',
+};
+
+export const MovieAPI = async (url = '', query = '') => {
+  const config = {
+    params: {
+      query: query,
+    },
+  };
+  const response = await axios.get(`${BASE_URL}${url}`, config);
+  return response.data;
 };
